c09ada3c45a69808fdab2396fe68804d569c5253
Nikola Markovic <nikola.markovicmc@gmail.com>
2026-02-27 21:14:32 +0100

    poboljsan support za reakcije izazvane sadrzajem

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,9 @@
 general_channel_id = "418750273977188354"
 etr_channel_id = "881581263155261491"
 admin_ids = [351451733857140736]
+content_reactions = {
+    "burek" : "🤓",
+    "zrno" : "<:zrnobraca:689878824611086360>",
+    "zidov" : "<:zidov:1380824800926236682>",
+    "XD" : "🤣"
+}
--- src/events.py
+++ src/events.py
@@ -2,16 +2,19 @@ import discord
 from discord.ext import commands as cms
 from discord import app_commands as acms
 
+import constants
+
 class Events(cms.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @cms.Cog.listener()
     async def on_message(self, ctx):
-
         if ctx.author == self.bot.user:
             return
         
-        if "burek" in ctx.content.lower():
-            await ctx.add_reaction("🤓")
-            return
+        # dict formata {sadrzaj : reakcija}
+        # za prevenciju stekovanja, dodati return na prvi prolaz petlje
+        for content, reaction in constants.content_reactions.items(): 
+            if content.lower() in ctx.content.lower():
+                await ctx.add_reaction(reaction)
